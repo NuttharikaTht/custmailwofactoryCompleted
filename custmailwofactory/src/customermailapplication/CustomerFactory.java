c1dca07a6bce07f983e3351742cc8cefff3738f5
NUTTHARIKA THONGTHAM <89317482+NuttharikaTht@users.noreply.github.com>
2021-09-04 00:51:20 +0700

    Add files via upload

---
+++ custmailwofactory/src/customermailapplication/CustomerFactory.java
@@ -0,0 +1,16 @@
+/* 62050154 Nuttharika Thongtham */
+
+package customermailapplication;
+
+public class CustomerFactory {
+    public static Customer createCustomer(String type) {
+        if (type.equals("Regular")) {
+            return new RegularCustomer();
+        } else if (type.equals("Mountain")) {
+            return new MountainCustomer();
+        } else if (type.equals("Delinquent")) {
+            return new DelinquentCustomer();
+        }
+        return null;
+    }
+}
